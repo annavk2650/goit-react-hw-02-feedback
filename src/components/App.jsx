d3e53b192a2b68031a03e455bb1eafb6da560ded
Anna Denysova <annavk2650@gmail.com>
2023-02-16 20:44:49 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification';
+import { Conteiner } from './Conteiner.styled';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = option => {
+    console.log(option);
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const result = good + neutral + bad;
+    return result;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const result = this.countTotalFeedback();
+    const totalPercentage = (good * 100) / result || 0;
+    return Math.round(totalPercentage);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const options = Object.keys(this.state);
+    return (
+      <>
+        <Conteiner>
+          <Section title="Please leave feedback">
+            <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback} />
+          </Section>
+
+          <Section title="Statistics">
+            {!total ? (
+              <Notification message="There is no feedback" />
+            ) : (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                positivePercentage={positivePercentage}
+              />
+            )}
+          </Section>
+        </Conteiner>
+      </>
+    );
+  }
+}
